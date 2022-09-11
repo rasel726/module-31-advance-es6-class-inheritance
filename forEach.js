f59a207e43821c93f12e27d00eb1a050758efc85
rasel726 <md.rasel2726@gmail.com>
2022-09-12 00:21:46 +0600

    31-4 Map string array, array of objects map, foreach

---
+++ forEach.js
@@ -0,0 +1,16 @@
+const friends = ['Tom Hanks', 'Tom Cruise', 'Tom Brady', 'Tom Solaiman'];
+
+friends.forEach(friend => {
+
+})
+
+const products = [
+    { id: 1, name: 'Laptop', price: 45000 },
+    { id: 1, name: 'Mobile', price: 80000 },
+    { id: 1, name: 'Watch', price: 45000 },
+    { id: 1, name: 'Tablet', price: 35000 },
+    { id: 1, name: 'Laptop', price: 23000 },
+];
+
+
+products.forEach(product => console.log(product));
